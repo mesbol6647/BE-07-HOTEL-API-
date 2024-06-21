93a27c804ff2794e3e757b1a6ce650ed25e365b1
Mesut <mesbol6647@gmail.com>
2024-06-21 18:41:09 +0300

    userId is not require

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -9,7 +9,7 @@ const ReservationSchema=new mongoose.Schema({
     userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
-        required:true
+        // required:true
 
     },
     
